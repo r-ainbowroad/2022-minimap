a545277c294eb85885f0d794a4d8e190864de57b
Octavia Togami <octavia.togami@gmail.com>
2022-04-03 02:03:42 -0700

    This is RGBA, not ARGB...

--- minimap.impl.user.js
+++ minimap.impl.user.js
@@ -528,7 +528,7 @@ const { html, render } = mlp_uhtml;
 
     rPlaceMask = new Array(maskCanvas.width * maskCanvas.height);
     for (let i = 0; i < rPlaceMask.length; i++) {
-      // Grayscale, pick red channel!
+      // Grayscale, pick green channel!
       rPlaceMask[i] = maskData[i * 4 + 1];
     }
   }
